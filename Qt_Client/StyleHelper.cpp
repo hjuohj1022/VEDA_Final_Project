b7867379e8cab57314a7c348594c548f37b51318
IAMSEONGWON <yunsw0402@naver.com>
2026-02-10 16:32:52 +0900

    Qt UI Upgrade

---
+++ Qt_Client/StyleHelper.cpp
@@ -0,0 +1,4 @@
+#include "StyleHelper.h"
+
+// Define static member
+bool StyleHelper::isDarkMode = true;
--- Qt_Client/main.cpp
+++ Qt_Client/main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "StyleHelper.h"
 
 #include <QApplication>
 
@@ -6,7 +7,9 @@ int main(int argc, char *argv[])
 {
     qputenv("QT_MEDIA_BACKEND", "ffmpeg");
     QApplication a(argc, argv);
+    a.setStyleSheet(StyleHelper::getMainStyle());
     MainWindow w;
+    w.setWindowTitle("Vision VMS");
     w.show();
     return a.exec();
 }
