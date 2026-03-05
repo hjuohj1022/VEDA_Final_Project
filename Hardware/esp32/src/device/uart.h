ee3b3dc7683ca6b90d536270c3c77ee5eda26f07
freezebee747 <red123iu@naver.com>
2026-03-05 11:00:45 +0900

    STM32 코드 추가, MQTT 연결 안정화, Teensy 코드 확장

--- Hardware/esp32/src/device/uart.h
+++ Hardware/esp32/src/device/uart.h
@@ -18,15 +18,18 @@
 #define PROTO_EXTRA     1
 #define TOTAL_PKT       (HEADER_LEN + PROTO_EXTRA + FRAME_BYTES)
 
-// UART 드라이버 내부 버퍼: 작게 유지 (4KB면 충분)
 #define UART_RX_BUF     4096
-// 청크 읽기 크기
 #define CHUNK_SIZE      1024
 
-extern volatile uint8_t   g_frame_ready;
-extern uint8_t           *g_frame_buf;
-extern uint8_t            g_offset_packet;
-extern SemaphoreHandle_t  g_frame_mutex;
+// 더블 버퍼
+#define NUM_BUFFERS     2
+extern uint8_t          *g_frame_bufs[NUM_BUFFERS];
+extern volatile int      g_write_idx;
+extern volatile int      g_read_idx;
+
+extern volatile uint8_t  g_frame_ready;
+extern uint8_t           g_offset_packet;
+extern SemaphoreHandle_t g_frame_mutex;
 
 void uartInit(void);
 void uartTask(void *arg);
--- Hardware/esp32/src/mqtt/mqtt.h
+++ Hardware/esp32/src/mqtt/mqtt.h
@@ -4,11 +4,19 @@
 #include "mqtt_client.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
-#include "certs/cert.h" 
+#include "certs/cert.h"
+#include <stdbool.h>
+
+#define CHUNK_PAYLOAD_SIZE  1024                              // 청크 데이터 크기
+#define CHUNK_HEADER_SIZE   4                                 // chunk_index(2) + total_chunks(2)
+#define CHUNK_MSG_SIZE      (CHUNK_HEADER_SIZE + CHUNK_PAYLOAD_SIZE)  // 전체 메시지 크기
+#define CHUNK_TOPIC         "lepton/frame/chunk"
 
 extern esp_mqtt_client_handle_t client;
+extern bool mqtt_connected;
 
-void mqttEventHandler( void *handler_args,esp_event_base_t base, int32_t event_id, void *event_data);
-esp_mqtt_client_handle_t mqttClient();
+void mqttEventHandler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+esp_mqtt_client_handle_t mqttClient(void);
+void mqttFrameTask(void *arg);
 
 #endif
